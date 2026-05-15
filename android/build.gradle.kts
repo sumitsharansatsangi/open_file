d18e5d0b29c0ae8d0af03eb7a115e66883668a2a
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-15 15:13:03 +0530

    code updated

---
+++ android/build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+}
+
+group = "com.crazecoder.openfile"
+version="1.0-SNAPSHOT"
+
+repositories {
+        google()
+        mavenCentral()
+    }
+
+
+android {
+    compileSdk=37
+    namespace ="com.crazecoder.openfile"
+    defaultConfig {
+        minSdk = 23
+    }
+}
